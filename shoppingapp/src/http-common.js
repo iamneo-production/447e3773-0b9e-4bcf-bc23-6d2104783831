471d7b287295e6c60666ab8bbf770fe95e7d65ee
ankitabhamare <ankitabhamare1@gmail.com>
2023-08-20 14:31:11 +0000

    shoppingapp

--- shoppingapp/src/http-common.js
+++ shoppingapp/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:3002",
+    baseURL: "https://ide-fdafaeeaddcdfeceedcfcdabadbcafd.project.examly.io/proxy/8080/",
     headers: {
         "Content-type": "application/json"
     }
